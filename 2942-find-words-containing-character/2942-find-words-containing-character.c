bd2f5550a45080c1fea0584ced7a36fb05953902
OpenSourceNewbie <112954025+Vani-code-sys@users.noreply.github.com>
2024-01-19 16:35:31 +0530

    Time: 12 ms (69.45%), Space: 7.2 MB (100.00%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.c
@@ -0,0 +1,28 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findWordsContaining(char** words, int wordsSize, char x, int* returnSize) {
+    int i =0;
+    int *returnArray = (int *)malloc(wordsSize * sizeof(int)); 
+    *returnSize = 0;
+    //printf ("#Words: %d\n", wordsSize);
+    int retCount = 0;
+    while (i< wordsSize){
+        //printf ("Index: %d\n", i);
+        //printf ("Word: %s\n", words[i]);
+        if (strchr(words[i], x) != NULL) {
+            //printf ("Reached here\n");
+            returnArray[retCount] = i;
+            //printf ("return Index: %d\n", retCount);
+            //printf ("returnArray: %d\n", returnArray[i]);
+            retCount++;
+        }            
+        i++;
+    }
+    *returnSize = retCount;
+    //printf ("Ret count:%d\n", *returnSize);
+    for (int i=0; i < retCount; i++) {
+        //printf ("Return Array: %d\n", returnArray[i]);
+    }
+    return returnArray;
+}
